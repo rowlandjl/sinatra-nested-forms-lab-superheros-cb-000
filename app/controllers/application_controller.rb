8197477a1bfa30ab7d599aa68feff5b5ceaa43bf
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-24 01:11:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do 
+      erb :super_hero
+    end 
+    
+    post '/teams' do 
+      @team.name = params[:team][:name]
+      @team.motto = params[:team][:motto]
+      
+      erb :team 
+    end 
 
 
 end
